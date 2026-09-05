3926bc63b73a974e5d705d8ddd6b364cc1061592
hmaciel <sacanagemisso7@gmail.com>
2026-09-05 13:53:14 -0300

    feat: load dedicated exam focus styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./exam-focus.css";
 import Providers from "@/components/Providers";
 import AppShell from "@/components/AppShell";
 
