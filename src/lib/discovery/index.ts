14564a4328e71cbe27197cde40db74b76ee58363
hmaciel <sacanagemisso7@gmail.com>
2026-09-10 20:01:37 -0300

    feat: expose discovery harvesting primitives

---
+++ src/lib/discovery/index.ts
@@ -0,0 +1,7 @@
+export * from "./catalogAdapter";
+export * from "./documentInventory";
+export * from "./engine";
+export * from "./fingerprint";
+export * from "./harvestRunner";
+export * from "./sourceRecipe";
+export * from "./types";
